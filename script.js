f9ad34a4dd23019064a2d3c2542a59e40b253b3a
ryanparketh <ryanpark@rocketmail.com>
2022-09-20 11:13:54 -0400

    added button and reformated the timeboxes

--- script.js
+++ script.js
@@ -9,3 +9,5 @@ const time3 = document.getElementById ('three');
 const time4 = document.getElementById ('four');
 const time5 = document.getElementById ('five');
 const saveBtnArr = document.querySelectorAll ('button');
+
+var currentDay = new Date();
