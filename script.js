86a64ca18ab1f9a62d672b5900201c97e9f5f8a1
ryanparketh <ryanpark@rocketmail.com>
2022-09-19 20:00:10 -0400

    added basic const for js and added basic time blocks

---
+++ script.js
@@ -0,0 +1,11 @@
+const time8 = document.getElementById ('eight');
+const time9 = document.getElementById ('nine');
+const time10 = document.getElementById ('ten');
+const time11 = document.getElementById ('eleven');
+const time12 = document.getElementById ('tweleve');
+const time1 = document.getElementById ('one');
+const time2 = document.getElementById ('two');
+const time3 = document.getElementById ('three');
+const time4 = document.getElementById ('four');
+const time5 = document.getElementById ('five');
+
