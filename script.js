23c525604fbfe9546221567f34c92e3acdb16cb0
ryanparketh <ryanpark@rocketmail.com>
2022-09-22 10:08:11 -0400

    adding local storage to retrive the message

--- script.js
+++ script.js
@@ -7,43 +7,37 @@ var notesEL = document.querySelectorAll('.note');
 //added time of day
 const timeofDay = moment().hours();
 const hour = $('.hour');
-const saveAct= $('.saveBtn');
+const saveAct = $('.saveBtn');
 
-// function abc(){
-// for(i=8; i< 18; i++){
-//     var currentNote = localStorage.getItem(`${i}`);
-//     if(currentNote){
+function abc() {
+    for (i = 8; i < 18; i++) {
+        $(this).next().children().text(localStorage.getItem(`todo ${i}`)|| '');
+        // var currentNote = localStorage.getItem('${i}');
+        // if (currentNote) {
 
-//     }
-// }
-
-// }
-// abc()
-
-//added to allow the time dictate what the color of each box will be
-hour.each (function (i){
-  let time =  parseInt($(this).parent().attr('id'));
-
-    if (time < timeofDay){ 
-        $(this).next().addClass('past');
+        }
     }
-    else if (time == timeofDay){
-        $(this).next().addClass('present');
 
-    }else{
-        $(this).next().addClass('future');
-    }
-});
+abc()
 
 //added local storage
-function saveNote(event){
+function saveNote(event) {
     var parentNode = event.target.parentNode;
     var noteEl = parentNode.querySelector('.note');
 
     localStorage.setItem(parentNode.id, noteEl.value)
 }
+//added to allow the time dictate what the color of each box will be
+hour.each(function (i) {
+    let time = parseInt($(this).parent().attr('id'));
 
-//added the ability to add
-saveButtonsEl.forEach(function(button){
-    button.addEventListener('click', saveNote);
+    if (time < timeofDay) {
+        $(this).next().addClass('past');
+    }
+    else if (time == timeofDay) {
+        $(this).next().addClass('present');
+
+    } else {
+        $(this).next().addClass('future');
+    }
 });
